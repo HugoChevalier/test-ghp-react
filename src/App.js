382c54453954f795f2948cd4b5fefc4866d716a0
Your Name <you@example.com>
2021-03-05 15:08:24 +0100

    'update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import { VictoryBar, VictoryChart, VictoryAxis } from 'victory';
+import data from './data'
+import Exemple from './Exemple'
+
 
 function App() {
+  console.log(data);
+
+  const gameName = data.items.map((game) => {
+    console.log(game)
+    console.log('================')
+    return game.name
+  });
+  console.log(gameName)
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className="title"> Dashboard</div>
+
+    <div className="ligne">
+      <Exemple />
+
+      
+      <div className="card">
+      Prix moyen par jeux
+
+      <VictoryChart domainPadding={20}>
+        <VictoryAxis
+          tickValues={[1, 2, 3, 4]}
+          tickFormat={gameName}
+        />
+        <VictoryAxis
+          dependentAxis
+          tickFormat={(x) => (`${x}€`)}
+        />
+        <VictoryBar
+          data={data.items}
+          x="name"
+          y="price_average"
+        />
+      </VictoryChart>
+
+      </div>
+      </div>
+
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,46 @@
+const data = {
+    "items": [
+      {
+        "name": "jeux 1",
+        "price_average": 12,
+        "quantity": 14
+      },
+          {
+        "name": "jeux 2",
+        "price_average": 14,
+        "quantity": 3
+      },
+          {
+        "name": "jeux 3",
+        "price_average": 5,
+        "quantity": 24
+      },
+          {
+        "name": "jeux 4",
+        "price_average": 12.8,
+        "quantity": 56
+      },
+          {
+        "name": "jeux 5",
+        "price_average": 7.90,
+        "quantity": 12
+      },
+          {
+        "name": "jeux 6",
+        "price_average": 6,
+        "quantity": 75
+      },
+          {
+        "name": "jeux 7",
+        "price_average": 27,
+        "quantity": 2
+      },
+          {
+        "name": "jeux 8",
+        "price_average": 4.5,
+        "quantity": 12
+      }
+      ]
+  }
+
+  export default data;
